018936a9b3c4c0eb4e536dd05dbb021a7c56cf48
Pedro Tammela <pctammela@gmail.com>
2019-11-01 11:29:19 -0300

    analytics

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+tracker=$(cat gtag.js)
+for f in $(find . | grep ".html"); do
+   wc=$(wc -l $f | awk '{print $1}')
+   echo $wc
+   n=$(($wc - 3))
+   echo $(head -n +$n $f) > $f
+   echo $tracker >> $f
+   echo "</body>" >> $f
+   echo "</html>" >> $f
+   sync
+done
+
