ecb0bf9f4fcbd5949b1aa3799928de327b908319
Pedro Tammela <pctammela@gmail.com>
2019-11-01 11:29:25 -0300

    fix style

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ for f in $(find . | grep ".html"); do
    wc=$(wc -l $f | awk '{print $1}')
    echo $wc
    n=$(($wc - 3))
-   echo $(head -n +$n $f) > $f
+   echo "$(head -n +$n $f)" > $f
    echo $tracker >> $f
    echo "</body>" >> $f
    echo "</html>" >> $f
