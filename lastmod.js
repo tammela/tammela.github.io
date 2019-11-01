933177451308d0b52a68d562b7ee0a61a3d68f08
Pedro Tammela <pctammela@gmail.com>
2019-11-01 11:29:29 -0300

    added last updated

---
+++ lastmod.js
@@ -0,0 +1,5 @@
+<font size="1">
+<script language="Javascript">
+document.write("last update: " + document.lastModified + "");
+</script>
+</font>
--- update.sh
+++ update.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 tracker=$(cat gtag.js)
-for f in $(find . | grep ".html"); do
+lastmod=$(cat lastmod.js)
+if [ -x "$*" ]; then
+   files=""
+   for arg in "$*"
+   do
+      files="$files $arg"
+   done
+else
+   files=$(find . | grep ".html")
+fi
+for f in $files
+do
    wc=$(wc -l $f | awk '{print $1}')
    echo $wc
    n=$(($wc - 3))
    echo "$(head -n +$n $f)" > $f
    echo $tracker >> $f
+   echo $lastmod >> $f
    echo "</body>" >> $f
    echo "</html>" >> $f
    sync
